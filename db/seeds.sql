9ec000222d1bfc5203f0c3cb888923db50cb2c1c
Max <mkligman1020@gmail.com>
2019-04-08 10:50:06 -0700

    initial structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUE ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUE ("Justify Your Masculinity Through SPICE");
+INSERT INTO burgers (burger_name) VALUE ("Patty Melt");
